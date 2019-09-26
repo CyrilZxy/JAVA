89286c845ba73f47d9458975412cb98f7f72755c
CyrilZxy <1151119105@qq.com>
2019-09-26 19:23:25 +0800

    java 给定两个int变量，交换变量的值。

---
+++ work1.java
@@ -0,0 +1,10 @@
+public class work1
+{
+	public static void main(String[]args)
+	{
+		int num1 = 10;
+		int num2 = 20;
+		System.out.printf("num1=%d\n",num2);
+		System.out.printf("num2=%d\n",num1);
+	}
+}
