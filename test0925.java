71ac7c3e680201545dacebf979a24ba24766fea4
CyrilZxy <1151119105@qq.com>
2019-09-26 19:12:11 +0800

    java 初学 第一个java程序  几个数据类型

---
+++ test0925.java
@@ -0,0 +1,35 @@
+public class test0925
+{
+	public static void main (String[]args)
+	{
+	
+		//System.out.println("hello java !");
+		//System.out.println("my name is:"+args[0]);
+		
+		int num1=10;
+		System.out.println(num1);
+		System.out.println(Integer.MAX_VALUE);		//int maxValue
+		System.out.println(Integer.MIN_VALUE);		//int minValue
+		
+		int maxValue = Integer.MAX_VALUE;
+		int minValue = Integer.MIN_VALUE;		//溢出循环
+		System.out.println(maxValue+1);			//maxValue+1
+		System.out.println(minValue-1);			//minValue-1
+	
+		long num2 = 10L;
+		System.out.println(num2);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		
+		double num3 = 1.0;
+		System.out.println(num3);
+		
+		float num4 = 1.0f;
+		System.out.println(num4);
+		
+		
+	}
+}
+
+
+
