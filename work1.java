b604ca1be3582910a9a97aa71746e5af6133eb95
CyrilZxy <54348995+CyrilZxy@users.noreply.github.com>
2019-10-10 17:02:07 +0800

    交换两变量的值

--- work1.java
+++ work1.java
@@ -1,10 +1,18 @@
+
+
+//交换两变量值 
+
 public class work1
 {
 	public static void main(String[]args)
 	{
 		int num1 = 10;
 		int num2 = 20;
-		System.out.printf("num1=%d\n",num2);
-		System.out.printf("num2=%d\n",num1);
+		int sum=num1+num2;
+		num1=sum-num1;
+		num2=sum-num2;
+		
+		System.out.printf("num1=%d\n",num1);
+		System.out.printf("num2=%d\n",num2);
 	}
 }
