27ad3dcc58776013a3653aa10357e900fe983362
CyrilZxy <54348995+CyrilZxy@users.noreply.github.com>
2020-02-13 23:38:16 +0800

    项目搭建初识

---
+++ OSMonitor/src/com/bitedu/gui/OSMonitorController.java
@@ -0,0 +1,12 @@
+package com.bitedu.gui;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
+public class OSMonitorController {
+    public void handleCPUSelectionChanged(Event event) {
+    }
+
+    public void handleSelectFile(ActionEvent actionEvent) {
+    }
+}
